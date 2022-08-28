29913a0280d62be221824584e2999291347244b7
Chesini Pablo <pchesini1@gmail.com>
2022-08-28 20:49:33 -0300

    modificando el boton

--- src/app/componentes/boton/boton.component.ts
+++ src/app/componentes/boton/boton.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+//importe en el componente el input
 @Component({
   selector: 'app-boton',
   templateUrl: './boton.component.html',
   styleUrls: ['./boton.component.css']
 })
 export class BotonComponent implements OnInit {
+  //importo el text y el imput del componente boton
+  @Input() text: string = "";
+  @Input() color: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onClick(){
+    console.log("hisiste click")
   }
 
 }
