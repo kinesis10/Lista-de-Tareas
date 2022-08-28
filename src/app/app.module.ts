f42abf8907dc874695a5e444c5410b79f317155a
Chesini Pablo <pchesini1@gmail.com>
2022-08-28 20:18:25 -0300

    Agregando boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { BotonComponent } from './componentes/boton/boton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BotonComponent
   ],
   imports: [
     BrowserModule
